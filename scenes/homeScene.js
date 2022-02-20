e7d10d44336ce55ab1659223452a4d6310d21b3f
Shuning Zhang <shuningz@usc.edu>
2022-02-20 05:21:39 -0800

    finish homescene collision

--- scenes/homeScene.js
+++ scenes/homeScene.js
@@ -19,8 +19,8 @@ var HomeScene = new Phaser.Class({
         this.load.image('start_button', 'assets/ui/buttons/B_BT_Start_single.png');
 
         // map
-        this.load.image("tiles","assets/interior_tileset.png");
-        this.load.tilemapTiledJSON('map',"assets/interior_map.json");
+        this.load.image("tiles2","assets/interior_tileset.png");
+        this.load.tilemapTiledJSON('map2',"assets/interior_map.json");
         
 
     },
@@ -33,27 +33,27 @@ var HomeScene = new Phaser.Class({
     },
     create: function() {
         // tilemaps
-        const map = this.make.tilemap({ key: "map" });
+        const map = this.make.tilemap({ key: "map2" });
 
         // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
         // Phaser's cache (i.e. the name you used in preload)
-        const tileset = map.addTilesetImage("interior_tileset", "tiles");
+        const tileset = map.addTilesetImage("interior_tileset", "tiles2");
 
         // Parameters: layer name (or index) from Tiled, tileset, x, y
         const underLayers = map.createStaticLayer("underLayers", tileset, 0, 0);
         const worldLayers2 = map.createStaticLayer("worldLayers2", tileset, 0, 0);
         const worldLayers = map.createStaticLayer("worldLayers", tileset, 0, 0);
-        
-        worldLayer.setCollisionByProperty({ collide: true });
-        worldLayer2.setCollisionByProperty({ collide: true });
+
+        worldLayers.setCollisionByProperty({ collide: true });
+        worldLayers2.setCollisionByProperty({ collide: true });
 
         createAnimation(this);
         associateKeyboardInputs(this);
         
         // add the main character
-        spawnMainCharacter(this, SCREEN_RIGHT / 4 - 30, SCREEN_BOTTOM / 2 - 80, 'idle_down', MAIN_CHARACTER_SCALE);
-        this.physics.add.collider( mainCharacter.character, worldLayer);
-        this.physics.add.collider( mainCharacter.character, worldLayer2);
+        spawnMainCharacter(this, 320.750, SCREEN_BOTTOM - 10, 'idle_down', MAIN_CHARACTER_SCALE);
+        this.physics.add.collider( mainCharacter.character, worldLayers);
+        this.physics.add.collider( mainCharacter.character, worldLayers2);
     },
     update: function() {
         processInput(this);
