45a706613ea859a6f1b8e14addc92783843d5abf
Shuning Zhang <shuningz@usc.edu>
2022-02-19 19:34:10 -0800

    add bounds for moving area

--- scenes/beginGameScene.js
+++ scenes/beginGameScene.js
@@ -6,6 +6,14 @@ const main_character_anim_keys = [
     'chopping_down', 'chopping_up', 'chopping_left', 'chopping_right',
     'watering_down', 'watering_up', 'watering_left', 'watering_right',
 ];
+const WALKING_SPEED = 1;
+const RUNNING_SPEED = 3;
+const SCREEN_LEFT = 0;
+const SCREEN_RIGHT = 1280;
+const SCREEN_TOP = 0;
+const SCREEN_BOTTOM = 720;
+const MAIN_CHARACTER_SCALE = 2;
+const MAIN_CHARACTER_SIZE = 16;
 
 var keyW;
 var keyS;
@@ -13,8 +21,6 @@ var keyA;
 var keyD;
 var currentPlayingAnim = 'idle_down';
 var cat;
-const WALKING_SPEED = 1;
-const RUNNING_SPEED = 3;
 var running = false;
 
 var BeginGameScene = new Phaser.Class({
@@ -44,150 +50,150 @@ var BeginGameScene = new Phaser.Class({
             key: 'idle_down',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 0, 1, 2, 3, 4, 5, 6, 7 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'idle_up',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 8, 9, 10, 11, 12, 13, 14, 15 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'idle_left',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 16, 17, 18, 19, 20, 21, 22, 23 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'idle_right',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 24, 25, 26, 27, 28, 29, 30, 31 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         // walk
         this.anims.create({
             key: 'walk_down',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 32, 33, 34, 35, 36, 37, 38, 39 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'walk_up',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 40, 41, 42, 43, 44, 45, 46, 47 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'walk_right',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 48, 49, 50, 51, 52, 53, 54, 55 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'walk_left',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 56, 57, 58, 59, 60, 61, 62, 63 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         // run
         this.anims.create({
             key: 'run_down',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 64, 65, 66, 67, 68, 69, 70, 71 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'run_up',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 72, 73, 74, 75, 76, 77, 78, 79 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'run_right',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 80, 81, 82, 83, 84, 85, 86, 87 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'run_left',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 88, 89, 90, 91, 92, 93, 94, 95 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         // tilling
         this.anims.create({
             key: 'tilling_down',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 96, 97, 98, 99, 100, 101, 102, 103 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'tilling_up',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 104, 105, 106, 107, 108, 109, 110, 111 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'tilling_left',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 112, 113, 114, 115, 116, 117, 118, 119 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'tilling_right',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 120, 121, 122, 123, 124, 125, 126, 127 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         // chopping
         this.anims.create({
             key: 'chopping_down',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 128, 129, 130, 131, 132, 133, 134, 135 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'chopping_up',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 136, 137, 138, 139, 140, 141, 142, 143 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'chopping_left',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 144, 145, 146, 147, 148, 149, 150, 151 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'chopping_right',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 152, 153, 154, 155, 156, 157, 158, 159 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         // watering
         this.anims.create({
             key: 'watering_down',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 160, 161, 162, 163, 164, 165, 166, 167 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'watering_up',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 168, 169, 170, 171, 172, 173, 174, 175 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'watering_left',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 176, 177, 178, 179, 180, 181, 182, 183 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         this.anims.create({
             key: 'watering_right',
             frames: this.anims.generateFrameNumbers('main_character', { frames: [ 184, 185, 186, 187, 188, 189, 190, 191 ] }),
             frameRate: 8,
-            repeat: -1
+            repeat: -1,
         });
         /** End of Main Characrer Animation Set **/
         /********************* End of Animation set *********************/
@@ -197,8 +203,8 @@ var BeginGameScene = new Phaser.Class({
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         var text = this.add.text(
-            640, 
-            360, 
+            SCREEN_RIGHT / 2,
+            SCREEN_BOTTOM / 2,
             "main game", 
             {
                 fontSize: 50,
@@ -215,8 +221,8 @@ var BeginGameScene = new Phaser.Class({
 
         this.input.on('gameobjectdown', this.onObjectClicked, this);
         // add the main character
-        cat = this.add.sprite(640, 360);
-        cat.setScale(2);
+        cat = this.add.sprite(SCREEN_RIGHT / 2, SCREEN_BOTTOM / 2);
+        cat.setScale(MAIN_CHARACTER_SCALE);
         cat.play('idle_down');
     },
     update: function() {
@@ -230,7 +236,8 @@ var BeginGameScene = new Phaser.Class({
                     currentPlayingAnim = 'run_up';
                     cat.play('run_up');
                 }
-                cat.y -= RUNNING_SPEED;
+                cat.y = Math.max(cat.y - RUNNING_SPEED,
+                    SCREEN_TOP + MAIN_CHARACTER_SCALE * MAIN_CHARACTER_SIZE / 2);
             }
             else
             {
@@ -240,7 +247,8 @@ var BeginGameScene = new Phaser.Class({
                     currentPlayingAnim = 'walk_up';
                     cat.play('walk_up');
                 }
-                cat.y -= WALKING_SPEED;
+                cat.y = Math.max(cat.y - WALKING_SPEED,
+                    SCREEN_TOP + MAIN_CHARACTER_SCALE * MAIN_CHARACTER_SIZE / 2);
             }
         }
         else if (keyS.isDown)
@@ -253,7 +261,8 @@ var BeginGameScene = new Phaser.Class({
                     currentPlayingAnim = 'run_down';
                     cat.play('run_down');
                 }
-                cat.y += RUNNING_SPEED;
+                cat.y = Math.min(cat.y + RUNNING_SPEED,
+                    SCREEN_BOTTOM - MAIN_CHARACTER_SCALE * MAIN_CHARACTER_SIZE / 2);
             }
             else
             {
@@ -263,7 +272,8 @@ var BeginGameScene = new Phaser.Class({
                     currentPlayingAnim = 'walk_down';
                     cat.play('walk_down');
                 }
-                cat.y += WALKING_SPEED;
+                cat.y = Math.min(cat.y + WALKING_SPEED,
+                    SCREEN_BOTTOM - MAIN_CHARACTER_SCALE * MAIN_CHARACTER_SIZE / 2);
             }
         }
         else if (keyA.isDown)
@@ -276,7 +286,8 @@ var BeginGameScene = new Phaser.Class({
                     currentPlayingAnim = 'run_left';
                     cat.play('run_left');
                 }
-                cat.x -= RUNNING_SPEED;
+                cat.x = Math.max(cat.x - RUNNING_SPEED,
+                    SCREEN_LEFT + MAIN_CHARACTER_SCALE * MAIN_CHARACTER_SIZE / 2);
             }
             else
             {
@@ -286,7 +297,8 @@ var BeginGameScene = new Phaser.Class({
                     currentPlayingAnim = 'walk_left';
                     cat.play('walk_left');
                 }
-                cat.x -= WALKING_SPEED;
+                cat.x = Math.max(cat.x - WALKING_SPEED,
+                    SCREEN_LEFT + MAIN_CHARACTER_SCALE * MAIN_CHARACTER_SIZE / 2);
             }
         }
         else if (keyD.isDown)
@@ -299,7 +311,8 @@ var BeginGameScene = new Phaser.Class({
                     currentPlayingAnim = 'run_right';
                     cat.play('run_right');
                 }
-                cat.x += RUNNING_SPEED;
+                cat.x = Math.min(cat.x + RUNNING_SPEED,
+                    SCREEN_RIGHT - MAIN_CHARACTER_SCALE * MAIN_CHARACTER_SIZE / 2);
             }
             else
             {
@@ -309,7 +322,8 @@ var BeginGameScene = new Phaser.Class({
                     currentPlayingAnim = 'walk_right';
                     cat.play('walk_right');
                 }
-                cat.x += WALKING_SPEED;
+                cat.x = Math.min(cat.x + WALKING_SPEED,
+                    SCREEN_RIGHT - MAIN_CHARACTER_SCALE * MAIN_CHARACTER_SIZE / 2);
             }
         }
         else
